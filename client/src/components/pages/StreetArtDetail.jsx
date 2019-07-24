6ffabe7bacc20357ab20990350db06f6b4465d0b
Maxence Bouret <maxence.bouret@gmail.com>
2019-07-24 16:05:49 +0100

    Iteration 13

--- client/src/components/pages/StreetArtDetail.jsx
+++ client/src/components/pages/StreetArtDetail.jsx
@@ -1,8 +1,13 @@
 import React, { useState, useEffect, useRef } from 'react'
 import api from '../../api'
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl'
 
 export default function StreetArtDetail(props) {
   const streetArtId = props.match.params.id
+
+  const mapDomRef = useRef(null) // For Mapbox
+  let map = useRef(null).current // For Mapbox
+  let marker = useRef(null).current // For Mapbox
 
   // imgEl is a reference to the <img> in the DOM
   const imgEl = useRef(null)
@@ -21,8 +26,28 @@ export default function StreetArtDetail(props) {
   useEffect(() => {
     api.getStreetArt(streetArtId).then(data => {
       setStreetArt(data)
+      let [lng, lat] = data.location.coordinates // For Mapbox
+      initMap(lng, lat) // For Mapbox
     })
   }, [])
+
+  function initMap(lng, lat) {
+    // Embed the map where "mapDomRef" is defined in the render
+    map = new mapboxgl.Map({
+      container: mapDomRef.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: 10,
+    })
+
+    // Add zoom control on the top right corner
+    map.addControl(new mapboxgl.NavigationControl())
+
+    // Create a marker on the map with the coordinates ([lng, lat])
+    marker = new mapboxgl.Marker({ color: 'red' })
+      .setLngLat([lng, lat])
+      .addTo(map)
+  }
 
   return (
     <div>
@@ -38,6 +63,8 @@ export default function StreetArtDetail(props) {
           <hr />
           Longitude = {streetArt.location.coordinates[0]} <br />
           Latitude = {streetArt.location.coordinates[1]} <br />
+          <hr />
+          <div ref={mapDomRef} style={{ height: 400 }} /> {/* NEW */}
         </div>
       )}
     </div>
--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom'
 import './index.scss'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './components/App.jsx'
-// import registerServiceWorker from './registerServiceWorker';
+
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl' // NEW
+
+// Inform your Mapbox token (https://www.mapbox.com/account/)
+mapboxgl.accessToken =
+  'pk.eyJ1IjoibWMxMDBzIiwiYSI6ImNqb2E2ZTF3ODBxa3czd2xldHp1Z2FxbGYifQ.U4oatm5RsTXXHQLz5w66dQ' // NEW
 
 ReactDOM.render(
   <Router>
@@ -11,4 +16,3 @@ ReactDOM.render(
   </Router>,
   document.getElementById('root')
 )
-// registerServiceWorker();
